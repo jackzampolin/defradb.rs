e89fc505e1bdce968ad1154f267573585f6ed076
Jack Zampolin <jack.zampolin@gmail.com>
2026-05-18 12:34:41 -0700

    feat(query): support multi-cid queries

--- crates/query-parse/src/query_parse/parser.rs
+++ crates/query-parse/src/query_parse/parser.rs
@@ -686,11 +686,6 @@ fn parse_field_to_select(
                 // null means "no cid filter" (skip setting it)
                 if !matches!(arg_value, Value::Null) {
                     let cids = parse_cid_value(arg_value, variables)?;
-                    if cids.len() > 1 {
-                        return Err(QueryError::parse(
-                            "querying by multiple cids is not yet supported",
-                        ));
-                    }
                     select.cid = Some(cids);
                 }
             }
